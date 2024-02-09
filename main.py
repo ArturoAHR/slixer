f5584893c089c31eb7f87693bf69ce93ea746c35
Arturo <arturoaureliohr@gmail.com>
2024-02-09 01:33:27 -0400

    Add basic slixer command argument parsing configuration

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+import argparse
+from commands.slixer.command import slixer
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="slixer",
+        description="A terminal audio file slicer that works by timestamps",
+    )
+    parser.set_defaults(func=slixer)
+
+    parser.add_argument("audio_file_path", help="Path to audio file")
+    parser.add_argument(
+        "-t",
+        "--timestamps-file",
+        dest="timestamp_file_path",
+        help="Path to text file with timestamps",
+    )
+
+    args = parser.parse_args()
+
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
