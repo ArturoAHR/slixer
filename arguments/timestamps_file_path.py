eb8c98a172ea3605d9688092175df6aa6d4f774f
Arturo <arturoaureliohr@gmail.com>
2024-02-10 17:28:37 -0400

    Add sorting and default titles to timestamp extraction

--- arguments/timestamps_file_path.py
+++ arguments/timestamps_file_path.py
@@ -1,4 +1,4 @@
-from utils import file
+from utils import file, time
 
 
 def validate(timestamps_file_path: str) -> bool:
@@ -13,11 +13,14 @@ def extract_timestamps(timestamps_flle_path: str) -> list:
     with open(timestamps_flle_path, "r") as file:
         lines = file.readlines()
 
-        for line in lines:
+        for index, line in enumerate(lines):
             timestamp_parts = line.split(" ", 1)
 
             timestamp = timestamp_parts[0]
-            song_title = timestamp_parts[1].strip()
+            song_title = f"Untitled Song {index + 1}"
+
+            if len(timestamp_parts) > 1:
+                song_title = timestamp_parts[1].strip()
 
             timestamp_splitted = timestamp.split(":")
 
@@ -36,5 +39,7 @@ def extract_timestamps(timestamps_flle_path: str) -> list:
                     "song_title": song_title,
                 }
             )
+
+    timestamps.sort(key=lambda x: time.convert_to_ms(x["timestamp"]))
 
     return timestamps
--- arguments/timestamps_file_path_test.py
+++ arguments/timestamps_file_path_test.py
@@ -1,11 +1,6 @@
 from unittest.mock import mock_open, patch
 from arguments.timestamps_file_path import validate, extract_timestamps
 
-mocked_timestamps_file_contents = """00:00:00 Song 1
-00:03:00 Song 2
-00:06:00 Song 3
-"""
-
 
 @patch("utils.file.verify_mime_type", return_value=True)
 @patch("utils.file.file_path_exists", return_value=True)
@@ -38,6 +33,11 @@ def test_validate_when_file_exists_but_its_not_text(
 
 
 def test_timestamp_extraction():
+    mocked_timestamps_file_contents = """00:00:00 Song 1
+00:03:00 Song 2
+00:06:00 Song 3
+"""
+
     with patch(
         "builtins.open", mock_open(read_data=mocked_timestamps_file_contents)
     ) as mocked_open:
@@ -49,5 +49,53 @@ def test_timestamp_extraction():
             {"timestamp": (0, 6, 0), "song_title": "Song 3"},
         ]
 
-        assert timestamps == expected_timestamps
+        assert (
+            timestamps == expected_timestamps
+        ), "Timestamps should have been extracted"
+        mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
+
+
+def test_timestamp_extraction_when_timestamps_are_unsorted():
+    mocked_timestamps_file_contents = """00:00:00 Song 1
+00:06:00 Song 3
+00:03:00 Song 2
+"""
+
+    with patch(
+        "builtins.open", mock_open(read_data=mocked_timestamps_file_contents)
+    ) as mocked_open:
+        timestamps = extract_timestamps("/fake/path/timestamps.txt")
+
+        expected_timestamps = [
+            {"timestamp": (0, 0, 0), "song_title": "Song 1"},
+            {"timestamp": (0, 3, 0), "song_title": "Song 2"},
+            {"timestamp": (0, 6, 0), "song_title": "Song 3"},
+        ]
+
+        assert (
+            timestamps == expected_timestamps
+        ), "Timestamps should be sorted by time"
+        mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
+
+
+def test_timestamp_extraction_when_timestamps_titles_are_missing():
+    mocked_timestamps_file_contents = """00:00:00 Song 1
+00:03:00 Song 2
+00:06:00
+"""
+
+    with patch(
+        "builtins.open", mock_open(read_data=mocked_timestamps_file_contents)
+    ) as mocked_open:
+        timestamps = extract_timestamps("/fake/path/timestamps.txt")
+
+        expected_timestamps = [
+            {"timestamp": (0, 0, 0), "song_title": "Song 1"},
+            {"timestamp": (0, 3, 0), "song_title": "Song 2"},
+            {"timestamp": (0, 6, 0), "song_title": "Untitled Song 3"},
+        ]
+
+        assert (
+            timestamps == expected_timestamps
+        ), "Missing titles should be replaced with 'Untitled Song {index}'"
         mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
