ae5d3dcb55f0a864cfde634261127c5acad7fac4
Arturo <arturoaureliohr@gmail.com>
2024-02-10 20:00:37 -0400

    Add test for handling unsupported formats

--- commands/slixer/utils_test.py
+++ commands/slixer/utils_test.py
@@ -28,3 +28,20 @@ def test_audio_file_splitting(mock_export_audio_file, mock_audio_segment):
         format = "mp3"
         expected_title = timestamp["song_title"]
         mock_export_audio_file.assert_any_call(ANY, expected_title, format)
+
+
+@patch("utils.file.export_audio_file")
+def test_audio_file_splitting_when_file_format_is_not_supported(
+    mock_export_audio_file, mock_audio_segment
+):
+    audio_file_path = "path/fake/audio.txt"
+    timestamps = [
+        {"start_time": (0, 0, 0), "song_title": "Intro"},
+        {"start_time": (0, 1, 0), "song_title": "Verse"},
+    ]
+
+    with pytest.raises(ValueError) as context:
+        split_audio_file(audio_file_path, timestamps)
+
+    assert "Unsupported file format: txt" in str(context.value)
+    mock_audio_segment.from_file.assert_not_called()
