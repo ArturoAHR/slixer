678a8bcc1499ab118e7dcbf5b51e6f329b7edcfb
Arturo <arturoaureliohr@gmail.com>
2024-02-15 14:29:21 -0400

    Add tests for new use cases timestamp extraction must cover

--- arguments/timestamps_file_path_test.py
+++ arguments/timestamps_file_path_test.py
@@ -55,6 +55,29 @@ def test_timestamp_extraction():
         mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
 
 
+def test_timestamp_extraction_when_segment_time_is_shortened():
+    mocked_timestamps_file_contents = """00:00 Segment 1
+03:00 Segment 2
+06:00 Segment 3
+"""
+
+    with patch(
+        "builtins.open", mock_open(read_data=mocked_timestamps_file_contents)
+    ) as mocked_open:
+        timestamps = extract_timestamps("/fake/path/timestamps.txt")
+
+        expected_timestamps = [
+            {"start_time": (0, 0, 0), "segment_title": "Segment 1"},
+            {"start_time": (0, 3, 0), "segment_title": "Segment 2"},
+            {"start_time": (0, 6, 0), "segment_title": "Segment 3"},
+        ]
+
+        assert (
+            timestamps == expected_timestamps
+        ), "Timestamps should have been extracted"
+        mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
+
+
 def test_timestamp_extraction_when_timestamps_are_unsorted():
     mocked_timestamps_file_contents = """00:00:00 Segment 1
 00:06:00 Segment 3
@@ -93,6 +116,121 @@ def test_timestamp_extraction_when_timestamps_titles_are_missing():
             {"start_time": (0, 0, 0), "segment_title": "Segment 1"},
             {"start_time": (0, 3, 0), "segment_title": "Segment 2"},
             {"start_time": (0, 6, 0), "segment_title": "Untitled Segment 3"},
+        ]
+
+        assert (
+            timestamps == expected_timestamps
+        ), "Missing titles should be replaced with 'Untitled Segment {index}'"
+        mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
+
+
+def test_timestamp_extraction_when_timestamps_have_time_between_characters():
+    mocked_timestamps_file_contents = """(00:00:00) Segment 1
+[00:03:00] Segment 2
+/00:06:00/ Segment 3
+"""
+
+    with patch(
+        "builtins.open", mock_open(read_data=mocked_timestamps_file_contents)
+    ) as mocked_open:
+        timestamps = extract_timestamps("/fake/path/timestamps.txt")
+
+        expected_timestamps = [
+            {"start_time": (0, 0, 0), "segment_title": "Segment 1"},
+            {"start_time": (0, 3, 0), "segment_title": "Segment 2"},
+            {"start_time": (0, 6, 0), "segment_title": "Segment 3"},
+        ]
+
+        assert (
+            timestamps == expected_timestamps
+        ), "Missing titles should be replaced with 'Untitled Segment {index}'"
+        mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
+
+
+def test_timestamp_extraction_when_timestamps_time_dash_characters():
+    mocked_timestamps_file_contents = """(00:00:00) - Segment 1
+[00:03:00] - Segment 2
+/00:06:00/ - Segment 3
+"""
+
+    with patch(
+        "builtins.open", mock_open(read_data=mocked_timestamps_file_contents)
+    ) as mocked_open:
+        timestamps = extract_timestamps("/fake/path/timestamps.txt")
+
+        expected_timestamps = [
+            {"start_time": (0, 0, 0), "segment_title": "Segment 1"},
+            {"start_time": (0, 3, 0), "segment_title": "Segment 2"},
+            {"start_time": (0, 6, 0), "segment_title": "Segment 3"},
+        ]
+
+        assert (
+            timestamps == expected_timestamps
+        ), "Missing titles should be replaced with 'Untitled Segment {index}'"
+        mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
+
+
+def test_timestamp_extraction_when_timestamps_have_time_at_the_end():
+    mocked_timestamps_file_contents = """Segment 1 00:00:00
+Segment 2 00:03:00
+Segment 3 00:06:00
+"""
+
+    with patch(
+        "builtins.open", mock_open(read_data=mocked_timestamps_file_contents)
+    ) as mocked_open:
+        timestamps = extract_timestamps("/fake/path/timestamps.txt")
+
+        expected_timestamps = [
+            {"start_time": (0, 0, 0), "segment_title": "Segment 1"},
+            {"start_time": (0, 3, 0), "segment_title": "Segment 2"},
+            {"start_time": (0, 6, 0), "segment_title": "Segment 3"},
+        ]
+
+        assert (
+            timestamps == expected_timestamps
+        ), "Missing titles should be replaced with 'Untitled Segment {index}'"
+        mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
+
+
+def test_timestamp_extraction_when_timestamps_time_between_characters_end():
+    mocked_timestamps_file_contents = """Segment 1 (00:00:00)
+Segment 2 [00:03:00]
+Segment 3 /00:06:00/
+"""
+
+    with patch(
+        "builtins.open", mock_open(read_data=mocked_timestamps_file_contents)
+    ) as mocked_open:
+        timestamps = extract_timestamps("/fake/path/timestamps.txt")
+
+        expected_timestamps = [
+            {"start_time": (0, 0, 0), "segment_title": "Segment 1"},
+            {"start_time": (0, 3, 0), "segment_title": "Segment 2"},
+            {"start_time": (0, 6, 0), "segment_title": "Segment 3"},
+        ]
+
+        assert (
+            timestamps == expected_timestamps
+        ), "Missing titles should be replaced with 'Untitled Segment {index}'"
+        mocked_open.assert_called_with("/fake/path/timestamps.txt", "r")
+
+
+def test_timestamp_extraction_when_timestamps_time_dash_characters_end():
+    mocked_timestamps_file_contents = """Segment 1 - (00:00:00)
+Segment 2 - [00:03:00]
+Segment 3 - /00:06:00/
+"""
+
+    with patch(
+        "builtins.open", mock_open(read_data=mocked_timestamps_file_contents)
+    ) as mocked_open:
+        timestamps = extract_timestamps("/fake/path/timestamps.txt")
+
+        expected_timestamps = [
+            {"start_time": (0, 0, 0), "segment_title": "Segment 1"},
+            {"start_time": (0, 3, 0), "segment_title": "Segment 2"},
+            {"start_time": (0, 6, 0), "segment_title": "Segment 3"},
         ]
 
         assert (
